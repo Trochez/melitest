51ed2b676b55783af8a35928ff2ef442a9c9cbe3
Juan Diego <tromon92@gmail.com>
2021-06-18 00:08:28 -0500

    update docker enviroments variables and frontend properties

--- src/main/resources/public/properties/properties.js
+++ src/main/resources/public/properties/properties.js
@@ -1,8 +1,11 @@
 properties={
 
-	baseurl:'localhost',
+	/*baseurl:'localhost',
 	protocol:'http://',
-	port:':8080',
+	port:':8080',*/
+	baseurl:${SERVICE_URL},
+	protocol:${SERVICE_POSRT},
+	port:${SERVICE_PROTOCOL},
 	name:'testmeli',
 		
 }
